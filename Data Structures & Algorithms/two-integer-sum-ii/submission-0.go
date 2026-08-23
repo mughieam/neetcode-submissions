787f7dfd1aa7e81537e597b95eea2c465781cf6d
Mughie Mughoni <mughieams24@gmail.com>
2026-08-23 21:02:08 +0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.go
@@ -0,0 +1,13 @@
+func twoSum(numbers []int, target int) []int {
+	l,r := 0, len(numbers)-1
+	for l<r {
+		if numbers[l] + numbers[r] > target {
+			r -= 1
+		} else if numbers[l] + numbers[r] < target {
+			l += 1
+		} else {
+			break
+		}
+	}
+	return []int{l+1,r+1}
+}
